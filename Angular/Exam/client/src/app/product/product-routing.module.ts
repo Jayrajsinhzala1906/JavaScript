ff9ac06c3127c913059d7f8c6ca973f05de3f43a
jayrajsinh <jayrajsinh.zala@kcsitglobal.com>
2022-04-18 17:09:31 +0530

    Added Product Routing

--- Angular/Exam/client/src/app/product/product-routing.module.ts
+++ Angular/Exam/client/src/app/product/product-routing.module.ts
@@ -1,10 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CartComponent } from './cart/cart.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
+import { ProductListComponent } from './product-list/product-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: ProductDetailsComponent,
+  },
+  {
+    path: ':id',
+    component: ProductListComponent,
+  },
+  {
+    path: '/cart',
+    component: CartComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class ProductRoutingModule { }
+export class ProductRoutingModule {}
